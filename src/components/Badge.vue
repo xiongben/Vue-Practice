8727675131551c142c658bb1ccd714a3bf7781d8
Xiong Ben <xiongben1993@gmail.com>
2020-04-30 23:47:27 +0800

    [add] css

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,9 +1,30 @@
 <template>
     <div class="box">
         <div class="badgeArea">
+           <SonComponent :detaildata="sondata"></SonComponent>
            
+           
+           <DetailBox3></DetailBox3>
+           <!-- <button @click="show = !show">
+                Toggle render
+            </button>
+            <div class="testan">
+           <transition name="bounce">
+                <DetailBox1 v-if="show"></DetailBox1>
+                <DetailBox2 v-if="!show"></DetailBox2>
+            </transition>
+            
+            </div> -->
+            <div class="testan">
+                <DetailBox1 class="testmovebox"></DetailBox1>
+                <DetailBox2></DetailBox2>
+            </div>
+            <div class="box1">
+                <div class="box2"></div>
+                <div class="box3"></div>
+            </div>
         </div>
-        <SonComponent :detaildata="sondata"></SonComponent>
+        
     </div>
 </template>
 
@@ -11,11 +32,15 @@
 import request from './../../system/api';
 import axios from "axios";
 import SonComponent from "./model/SonComponent";
+import DetailBox1 from "./model/DetailBox1";
+import DetailBox2 from "./model/DetailBox2";
+import DetailBox3 from "./model/DetailBox3";
 export default {
     name: 'Badge',
 
     data(){
       return {
+          show: false,
           title: "test badge",
           userId: 120058,
           achieveList: [],
@@ -26,7 +51,7 @@ export default {
           }
       }
     },
-    components:{SonComponent},
+    components:{SonComponent, DetailBox1, DetailBox2, DetailBox3},
     created() {
         // this.userId = this.getUrlparams(this.userId)? this.getUrlparams(this.userId):120058;
         // this.getBadge(this.userId);
@@ -72,61 +97,122 @@ export default {
 </script>
 
 <style scoped>
+.box1{
+    width: 200px;
+    height: 50px;
+    background: blue;
+    overflow: hidden;
+    position: relative;
+}
+.box2{
+    width: 150px;
+    height: 50px;
+    background: yellow;
+    position: absolute;
+    z-index: 4;
+    left: 0;
+    top:0;
+    animation: movetest 4s forwards;
+}
+.box3{
+    width: 150px;
+    height: 50px;
+    background: red;
+    position: absolute;
+    z-index: 3;
+    left: 150px;
+    top:0;
+}
+.testan {
+    width: 100%;
+    height: 2.5rem;
+    position: relative;
+    overflow: hidden;
+}
    .box{
-       width: 3.75rem;
+       width: 100%;
+       height: 100vh;
        overflow: hidden;
+       background: pink;
    }
    .badgeArea{
     width: 100%;
+   }
+   .slide-fade-enter-active {
+  transition: all .3s ease;
+    }
+    .slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active for below version 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+    }
+  
+  .bounce-enter-active {
+  animation: bounce-in .5s;
 }
-.badgeTitle {
-    height: 0.34rem;
-    line-height: 0.34rem;
-    margin-top: 0.1rem;
-    font-size: 14px;
-    color: rgb(176, 176, 187);
-    font-family: Roboto, Helvetica, sans-serif;
-    font-weight: bolder;
-    /* background: yellow; */
-    width: 3.45rem;
-    margin:0 auto;
-    /* padding-left: 0.15rem; */
-    text-align: left;
+.bounce-leave-active {
+  /* animation: bounce-in .5s reverse; */
+  animation: bounce-in .5s ;
 }
-.badgeUl{
-    width: 3.6rem;
-    margin: 0 auto;
-    overflow: hidden;
+@keyframes bounce-in {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(50%);
+  }
+  100% {
+    transform: translate(100%);
+  }
 }
-.badgeLi{
-    width: 1.2rem;
-    height: 1.4rem;
-    float: left;
-    text-align: center;
+
+@keyframes bounce-out {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(50%);
+  }
+  100% {
+    transform: translate(100%);
+  }
 }
-.badgeIcon{
-    width: 0.88rem;
-    height: 0.88rem;
-    /* background: red; */
-    margin-top: 0.1rem;
+
+@keyframes movetest {
+    0% {
+    left: 0;
+    top:0px;
+  }
+  50% {
+    left: 50%;
+    top:0px;
+  }
+  100% {
+    left: 100%;
+     top:0px;
+  }
 }
-.textDetail{
-    height: 20px;
-    width: 100%;
-    line-height: 20px;
-    font-size: 10px;
-    color: rgb(44,44,48);
-    font-family: Roboto, Helvetica, sans-serif;
-    overflow: hidden;
-    text-overflow:ellipsis;
-    white-space: nowrap;
+
+@keyframes example {
+ 0%   {background-color:red; left:0px; top:0px;}
+  25%  {background-color:yellow; left:50%; top:0px;}
+  50%  {background-color:blue; left:50%; top:200px;}
+  75%  {background-color:green; left:0px; top:200px;}
+  100% {background-color:red; left:0px; top:0px;}
 }
-.otherText{
-    height: 20px;
-    line-height: 20px;
-    font-size: 12px;
-    color: rgb(171,173,187);
-    font-family: Roboto, Helvetica, sans-serif;
+.movebox{
+    width: 100px;
+    height: 100px;
+  animation-name: example;
+  animation-duration: 4s;
+  position: relative;
+}
+.testmovebox {
+  animation: movetest 4s forwards;
+  
 }
 </style>
 
---
+++ src/components/model/DetailBox2.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="sbox">
+      <h1>this is box 2</h1>
+      <p class="text1">{{title}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'DetailBox2',
+    props: {
+       detaildata: {},
+    },
+    data() {
+        return {
+            title: "box2"
+        }
+    },
+    created() {
+        
+    },
+    mounted() {
+       
+        
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style scoped>
+   .sbox {
+     width: 3.75rem;
+     height: 2.5rem;
+     background: rgb(117, 214, 221);
+     position: absolute;
+     left: 0;
+     top: 0;
+   }
+   .text1 {
+      font-size: 18px;
+   }
+</style>
